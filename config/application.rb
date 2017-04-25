5f24cedbc6d3d30ced6b73c1fdaf4b740952bd84
Gyula Pinter <v-gpinter@hotels.com>
2017-04-25 15:07:40 +0200

    fix heroku

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ module PersonalAssetCollector
     # -- all .rb files in that directory are automatically loaded.
 
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components", "angular")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
-
   end
 end
