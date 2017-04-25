cbe98611bafb5a060df2604eb7b7bcd51470348c
Gyula Pinter <v-gpinter@hotels.com>
2017-04-25 16:25:18 +0200

    fix heroku

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,13 @@ module PersonalAssetCollector
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","app", "bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components", "angular")
     config.assets.paths << Rails.root.join("vendor","assets","app", "bower_components", "angular")
+    config.assets.paths << Rails.root.join("vendor","assets","app", "bower_components","bootstrap-sass-official")
+    config.assets.paths << Rails.root.join("vendor","assets","app", "bower_components","bootstrap-sass-official", "assets")
     config.assets.paths << Rails.root.join("vendor","assets","app", "bower_components","bootstrap-sass-official","assets","fonts")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+    config.assets.paths << Rails.root.join("vendor","assets","app", "bower_components","bootstrap-sass-official","assets","stylesheets")
+    config.assets.paths << Rails.root.join("vendor","assets","app", "bower_components","bootstrap-sass-official","assets","javascripts")
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
     config.public_file_server.enabled = true
     config.assets.serve_static_files = true
